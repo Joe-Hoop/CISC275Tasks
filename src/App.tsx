4171436cddf7a65a194463166691abb1d4061b06
J-Hooper <joeyhoop@udel.edu>
2023-02-14 21:04:55 -0500

    Added a container with one row and two columns, containing an image with a brief description. Also added a list with three items.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-
+import { Container, Row, Col } from "react-bootstrap";
+import cartoon from "./assets/orange.png";
 function App(): JSX.Element {
     return (
         <div className="App">
@@ -13,6 +14,25 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <Container>
+                <Row>
+                    <Col>Behold a random image I found on my computer:</Col>
+                    <Col>
+                        <img
+                            src={cartoon}
+                            alt="A cartoon orange with sunglasses"
+                            width={"20%"}
+                            height={"auto"}
+                        />
+                    </Col>
+                </Row>
+            </Container>
+            <ol>
+                My favorite foods are:
+                <li>Turkey Bacon</li>
+                <li>Sirloin Steaks</li>
+                <li>Chocolate</li>
+            </ol>
         </div>
     );
 }
