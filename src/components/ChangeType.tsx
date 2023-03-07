8c1cb80a761f5f00a3e03f71ba4fd9ae55190137
J-Hooper <joeyhoop@udel.edu>
2023-03-07 16:47:21 -0500

    Solved ChangeType and RevealAnswer components

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,20 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+    const q_changer = (theType: QuestionType) => {
+        theType === "short_answer_question"
+            ? setType("multiple_choice_question")
+            : setType("short_answer_question");
+    };
+    return (
+        <div>
+            <Button onClick={() => q_changer(type)}>Change Type</Button>
+            {type === "short_answer_question" ? (
+                <span>Short Answer</span>
+            ) : (
+                <span>Multiple Choice</span>
+            )}
+        </div>
+    );
 }
