58ed7f9da3a2e975d3974127f4c6ff8be38ef4f3
J-Hooper <joeyhoop@udel.edu>
2023-03-07 21:27:51 -0500

    Solved CycleHoliday component

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,32 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    type favHoliday = "🐇" | "🇺🇸" | "👻" | "🦃" | "🎅🏻";
+    const [holiday, setHoliday] = useState<favHoliday>("🎅🏻");
+    const dates_record: Record<favHoliday, favHoliday> = {
+        "🐇": "🇺🇸",
+        "🇺🇸": "👻",
+        "👻": "🦃",
+        "🦃": "🎅🏻",
+        "🎅🏻": "🐇"
+    };
+    const alphabet_record: Record<favHoliday, favHoliday> = {
+        "🎅🏻": "🐇",
+        "🐇": "🇺🇸",
+        "🇺🇸": "👻",
+        "👻": "🦃",
+        "🦃": "🎅🏻"
+    };
+
+    return (
+        <div>
+            <Button onClick={() => setHoliday(alphabet_record[holiday])}>
+                Advance by Alphabet
+            </Button>
+            <Button onClick={() => setHoliday(dates_record[holiday])}>
+                Advance by Year
+            </Button>
+            <span>Holiday: {holiday}</span>
+        </div>
+    );
 }
