18faf33a094dcc934bed551aaa76511deceb4ecf
J-Hooper <joeyhoop@udel.edu>
2023-03-16 13:04:56 -0400

    Solved ChangeColor and MultipleChoiceQuestion components

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,47 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
+    const COLORS = [
+        "red",
+        "blue",
+        "green",
+        "orange",
+        "purple",
+        "yellow",
+        "pink",
+        "brown"
+    ];
+    const [colorIndex, setColorIndex] = useState<number>(0);
+    const updateColor = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const index = COLORS.indexOf(event.target.value);
+        setColorIndex(index);
+    };
     return (
         <div>
             <h3>Change Color</h3>
+            {COLORS.map((color: string) => (
+                <Form.Check
+                    inline
+                    key={color}
+                    type="radio"
+                    name="colors"
+                    onChange={updateColor}
+                    id={color}
+                    label={color}
+                    value={color}
+                    checked={color === COLORS[colorIndex]}
+                />
+            ))}
+            <div>
+                <span>You have chosen </span>
+                <span
+                    data-testid="colored-box"
+                    style={{ backgroundColor: COLORS[colorIndex] }}
+                >
+                    {COLORS[colorIndex]}
+                </span>
+            </div>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,26 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>(options[0]);
+    const updateAnswer = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setAnswer(event.target.value);
+    };
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <div>
+                <Form.Group controlId="multipleChoice">
+                    <Form.Label>Select An Answer</Form.Label>
+                    <Form.Select value={answer} onChange={updateAnswer}>
+                        {options.map((choice: string) => (
+                            <option key={choice} value={choice}>
+                                {choice}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+            </div>
+            {answer === expectedAnswer ? <p>✔️</p> : <p>❌</p>}
         </div>
     );
 }
